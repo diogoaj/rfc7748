ffb651e47257bfc365def612b2701463c2862dc8
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2019-07-27 15:11:13 +0100

    Add point addition function

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,14 +1,38 @@
 use rug::Integer;
-
-// Curve 25519 used for ECDH
+use rug::ops::Pow;
 
 // Montgomery Curve
-// y^2 = x^3 + 486662x^2 + x mod 2^252 + 27742317777372353535851937790883648493
+// y^2 = x^3 + 486662x^2 + x mod 2^255 - 19
 pub struct Curve25519{
 	pub a: Integer,
 	pub b: Integer,
 	pub prime: Integer,
+	pub base: Point,
 }
+
+#[derive(Clone)]
+pub struct Point{
+	pub x: Integer,
+	pub y: Integer,
+}
+
+impl Point{
+	pub fn new(x: Integer, y: Integer) -> Point{
+		Point{
+			x,
+			y,
+		}
+	}
+
+	pub fn getx(&self) -> &Integer{
+		return &self.x;
+	}
+
+	pub fn gety(&self) -> &Integer{
+		return &self.y;
+	}
+}
+
 
 impl Curve25519{
 	pub fn new() -> Curve25519{
@@ -16,12 +40,37 @@ impl Curve25519{
 			a: Integer::from(486662), 
 			b: Integer::from(1),
 			prime: Integer::from(Integer::u_pow_u(2, 255)) - Integer::from(19), 
+			base: Point::new(Integer::from(9), 
+							Integer::from("14781619447589544791020593568409986887264606134616475288964881837755586237401".parse::<Integer>().unwrap())),
 		}
 	}
-}
 
+	// From https://en.wikipedia.org/wiki/Montgomery_curve#Addition
+	pub fn point_add(&self, p1: &Point, p2: &Point) -> Point{
+		// Calculate x3 coordinate
+		let mut num = (Integer::from(p2.getx()*p1.gety()) - p1.getx()*p2.gety()).square();
+		let mut den = Integer::from(p1.getx()*p2.getx())*(Integer::from(p2.getx() - p1.getx()).square());
+		let mut inv = den.invert(&self.prime).unwrap();
 
-pub struct Point{
-	pub x: Integer,
-	pub y: Integer,
+		let mut dividend = num*inv;
+		let mut r = dividend.div_rem_floor_ref(&self.prime); // Mod operation
+		let (_, x3) = <(Integer, Integer)>::from(r);         // Get remainder
+
+		// Calcualte y3 coordinate
+		num = (Integer::from(2*p1.getx() + p2.getx()) + &self.a)*Integer::from(p2.gety() - p1.gety());
+		inv = Integer::from(Integer::from(p2.getx() - p1.getx()).invert_ref(&self.prime).unwrap());
+
+		let tmp1 = num*inv;
+
+		num = Integer::from(p2.gety() - p1.gety()).pow(3);
+		den = Integer::from(p2.getx() - p1.getx()).pow(3);
+		inv = den.invert(&self.prime).unwrap();
+
+		let tmp2 = num*inv;
+		dividend = Integer::from(tmp1 - tmp2 - p1.gety());
+		r = dividend.div_rem_floor_ref(&self.prime); // Mod operation
+		let (_, y3) = <(Integer, Integer)>::from(r); // Get remainder
+
+		return Point::new(x3, y3);		
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use rug::Integer;
 mod crypto;
 
+use rug::Integer;
+
 fn main(){
-	let p = crypto::Point{
-		x: Integer::from(2),
-		y: Integer::from(3),
-	};
+	println!("Welcome to ecc implementation of Curve25519");
 
-	println!("X: {}, Y: {}", p.x, p.y);
-
+	// Test
 	let curve = crypto::Curve25519::new();
+	let p1 = &curve.base;
+	let p2 = crypto::Point::new(Integer::from("14847277145635483483963372537557091634710985132825781088887140890597596352251".parse::<Integer>().unwrap()),
+							    Integer::from("8914613091229147831277935472048643066880067899251840418855181793938505594211".parse::<Integer>().unwrap()));
 
-	println!("Prime number: {}", curve.prime);
+	println!("{}", curve.point_add(&p1, &p2).gety());
 }
