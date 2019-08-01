1a46194c4a7667ec06ec7f197fc3ae87f2eb15de
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2019-08-01 23:36:18 +0100

    Refactor crypto module

---
+++ src/crypto/ec.rs
@@ -0,0 +1,162 @@
+use rug::Integer;
+use rug::ops::Pow;
+
+pub struct KeyPair{
+	public_key: Vec<u8>,
+	private_key: Vec<u8>,
+}
+
+impl KeyPair{
+	/*
+	pub fn new() -> KeyPair{
+		KeyPair{
+			// TODO: generate 32 bytes for private key
+			// and generate public key
+		}
+	}*/
+
+	pub fn get_public_key(&self) -> &Vec<u8>{
+		return &self.public_key;
+	}
+
+	pub fn get_private_key(&self) -> &Vec<u8>{
+		return &self.private_key;
+	}
+
+	pub fn dh_exchange(&self, curve: &Curve25519, private_key: &Vec<u8>) -> Vec<u8>{
+		return curve.scalar_multiply(self.get_private_key(), private_key);
+	}
+}
+
+
+// Montgomery Curve
+// y^2 = x^3 + 486662x^2 + x mod 2^255 - 19
+pub struct Curve25519{
+	pub a: Integer,
+	pub b: Integer,
+	pub prime: Integer,
+}
+
+impl Curve25519{
+	pub fn new() -> Curve25519{
+		Curve25519{ 
+			a: Integer::from(486662), 
+			b: Integer::from(1),
+			prime: Integer::from(2).pow(255) - 19, 
+		}
+	}
+
+	// From RFC 7748 - Elliptic Curves for Security
+	// ============================================
+	pub fn decode_little_endian(&self, b: &Vec<u8>, bits: u32) -> Integer{
+		let mut sum = Integer::from(0);
+		for i in 0..(bits+7)/8 {
+			sum += Integer::from(b[i as usize]) << 8*i;
+		}
+
+		return sum;
+	}
+
+	pub fn decode_u_coordinate(&self, u: &mut Vec<u8>, bits: u32) -> Integer{
+		let size = u.len() - 1;
+		if bits % 8 == 0{
+			u[size] &= (1<<(bits%8))-1;
+		}
+		return self.decode_little_endian(u, bits);
+	}
+
+	pub fn encode_u_coordinate(&self, u: &mut Integer, bits: u32) -> Vec<u8>{
+		let mut arr = vec![0; (bits as usize + 7)/8];
+
+		let r = u.div_rem_floor_ref(&self.prime);
+		let (_, u) = <(Integer, Integer)>::from(r);     
+
+		let u = &Integer::from(u);
+
+		for i in 0..arr.len(){
+			arr[i] = (Integer::from(u >> 8*i as u32) & Integer::from(0xff)).to_u8().unwrap();
+		}
+
+		return arr;
+	}
+
+	pub fn decode_scalar_25519(&self, k: &mut Vec<u8>) -> Integer{
+		k[0] &= 248;
+		k[31] &= 127;
+		k[31] |= 64;
+
+		return self.decode_little_endian(k, 255);
+	}
+
+	pub fn cswap(&self, swap: &Integer, x_2: &Integer, x_3: &Integer) -> (Integer, Integer) {
+		let mask = Integer::from(0 - swap);
+
+		let dummy = mask & Integer::from(x_2^x_3);
+
+		let xx_2 = Integer::from(x_2^&dummy);
+		let xx_3 = Integer::from(x_3^&dummy);
+		
+		return (xx_2, xx_3)
+	}
+
+	pub fn scalar_multiply(&self, k: &Vec<u8>, u: &Vec<u8>) -> Vec<u8> {
+		let mut k_tmp = k.clone();
+		let mut u_tmp = u.clone();
+
+		let kk = self.decode_scalar_25519(&mut k_tmp);
+		let uu = self.decode_u_coordinate(&mut u_tmp, 255);
+
+		let a24 = Integer::from(121665);  
+		let p = Integer::from(2).pow(255) - Integer::from(19);
+
+		let x_1 = uu.clone();
+		let mut x_2 = Integer::from(1);
+		let mut z_2 = Integer::from(0);
+		let mut x_3 = uu.clone();
+		let mut z_3 = Integer::from(1);
+		let mut swap = Integer::from(0);
+
+		for t in (0..255).rev(){
+			let k_t = Integer::from(&kk >> t) & Integer::from(1);
+
+			swap = Integer::from(&swap ^ &k_t);
+
+			let (tx_2, tx_3) = self.cswap(&swap, &x_2, &x_3);
+			let (tz_2, tz_3) = self.cswap(&swap, &z_2, &z_3);
+
+			x_2 = tx_2;
+			x_3 = tx_3;
+			z_2 = tz_2;
+			z_3 = tz_3;
+
+			swap = k_t;
+
+			let two = Integer::from(2);
+
+			let a = Integer::from(&x_2 + &z_2);
+			let aa = Integer::from(a.pow_mod_ref(&two, &self.prime).unwrap());
+			let b = Integer::from(&x_2 - &z_2);
+			let bb = Integer::from(b.pow_mod_ref(&two, &self.prime).unwrap());
+			let e = Integer::from(&aa - &bb);
+			let c = Integer::from(&x_3 + &z_3);
+			let d = Integer::from(&x_3 - &z_3);
+			let da = Integer::from(&d * &a);
+			let cb = Integer::from(&c * &b);
+
+			x_3 = Integer::from(Integer::from(&da + &cb).pow_mod_ref(&two, &self.prime).unwrap());
+			z_3 = &x_1*Integer::from(Integer::from(&da - &cb).pow_mod_ref(&two, &self.prime).unwrap());
+			x_2 = Integer::from(&aa * &bb);
+			z_2 = &e*Integer::from(&aa + &a24 * &e);
+		}
+
+		let (tx_2, _) = self.cswap(&swap, &x_2, &x_3);
+		let (tz_2, _) = self.cswap(&swap, &z_2, &z_3);
+
+		x_2 = tx_2;
+		z_2 = tz_2;
+
+		let mut res = (x_2 * (z_2.pow_mod(&Integer::from(&p-2), &p).unwrap())) % &self.prime;
+
+		return self.encode_u_coordinate(&mut res, 255);
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 mod crypto;
 
+use rug::Integer;
+
 fn main(){
+	let curve = crypto::ec::Curve25519::new();
 	let mut k = hex::decode("0900000000000000000000000000000000000000000000000000000000000000").unwrap();
 	let mut u = hex::decode("0900000000000000000000000000000000000000000000000000000000000000").unwrap();
 	let mut r;
 
 	// Second iteration test ( 1000 iterations )
 	// TODO: implement test module
-	for _ in 0..1000{
-		let tmp_k = k.clone();
-		r = crypto::x25519(&mut k, &mut u);
-		u = tmp_k;
-		k = crypto::encode_u_coordinate(&mut r, 255);
-	}
-	
-	println!("{}", hex::encode(k));
+
+	r = curve.scalar_multiply(&mut k, &mut u);
+	println!("{}", hex::encode(r));
 }
